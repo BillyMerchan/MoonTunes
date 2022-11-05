e05ad6612b95dabe2a19534caf8a499025f2b9c4
Billy Merchan <billymerchan@gmail.com>
2022-11-05 07:08:57 -0400

    Fixing Routes Again :(

---
+++ my-app/src/Routes/About/about.js
@@ -0,0 +1,18 @@
+import './about.css';
+
+function About() {
+  return (
+    <div>
+      <div className="Page">
+        <header className="App-header">
+          <h1>About Page</h1>
+        </header>
+        <div className="Body">
+          <p>lorem ipsum</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
