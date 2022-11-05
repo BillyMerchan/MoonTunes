9257e69e12c09fe47d2523e4864de70a81ce9380
Billy Merchan <billymerchan@gmail.com>
2022-11-05 04:40:38 -0400

    Escape Menu Init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import './Styles/App.css';
 import {Howl, Howler} from 'howler';
+import EscapeMenu from './Components/EscapeMenu/EscapeMenu.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <EscapeMenu/>
       </header>
     </div>
   );
